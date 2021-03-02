d8110e74916f84956aac8a6793dd6ba52e22f433
indianajim <jimrunkel@gmail.com>
2021-03-01 21:47:05 -0500

    more 5hr py

--- 5hrPython/one.py
+++ 5hrPython/one.py
@@ -1,1 +1,37 @@
 print("Hi Kelly")
+print("my age is", 25)
+
+name = "Jim"
+
+print (name, "is my name")
+print (name, "is 25 yrs old")
+print (name)
+print ("John is my name")
+print ("John is 25 yrs old")
+
+age = 30
+print (name, "is", age, "yrs old")
+
+temperature1 = 95.5
+temperature2 = 94.5
+average = (temperature1 + temperature2)/2
+print(int(average))
+
+#str() convert string
+#int() convert to int
+#float() convert to float
+#bool() convert to bool
+
+digit_one = 1
+digit_two = 2
+
+print(str(digit_one) + str(digit_two))
+
+weather = input("how is the weather?")
+print(weather)
+
+current_year = 2021
+answer = input("age?")
+
+print("your birth year is", current_year - int(answer))
+
