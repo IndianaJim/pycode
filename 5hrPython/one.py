c6749c80c106c14fd824836776f27eaf9e6ab79c
indianajim <jimrunkel@gmail.com>
2021-03-08 17:51:25 -0500

    methods practice 1

--- 5hrPython/one.py
+++ 5hrPython/one.py
@@ -12,3 +12,30 @@ current_year = 2021
 print("current year", current_year, ". Next year is", current_year + 1)
 print(f"Current year is {current_year}. Next year is {current_year + 1}")
 
+name = "Jim doe"
+print(name.title())
+age = 25
+print(f"Title is: {name.title()}")
+print(name)
+name = name.title()
+print(name)
+
+#string methods
+#.title()
+#.capitalize()
+#.upper()
+#.lower()
+
+print(f"Title: {name.title()}")
+print(f"Capitalize: {name.capitalize()}")
+print(f"Upper: {name.upper()}")
+print(f"Lower: {name.lower()}")
+
+#divide without remainder 3 // 2
+#divide 3 / 2
+#modulus 3 % 2
+
+action = 3 // 2
+print(f'Result is: {action}')
+print(f'Result is: {type(action)}')
+print(f'Result is: {type(action)}')
