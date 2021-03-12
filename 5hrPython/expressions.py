d72693cc1edbfc3d99e18154094b8d386ebf2d9c
indianajim <jimrunkel@gmail.com>
2021-03-11 21:14:59 -0500

    practice

--- 5hrPython/expressions.py
+++ 5hrPython/expressions.py
@@ -3,4 +3,21 @@ b = 'John' != 'john'
 print(a)
 print(b)
 
+grade_john = 70
+if grade_john > 70:
+    print("Congrats! you've passed")
+else:
+    print("sorry you have not passed")
+print("good luck in the next exam")
+
+student_name = "john michael"
+is_snowing = True
+
+if is_snowing:
+    print(f"Hi {student_name},\nSince it is snowing today,\nschool is cancelled.")
+else:
+    print(f"Hi {student_name},\nSince it is not snowing today,\nschool will be open.")
+
+
+
 
