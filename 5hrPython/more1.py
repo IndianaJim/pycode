7ba43fe7e034c62838f5be6e1aaf0b80c4ae5eba
indianajim <jimrunkel@gmail.com>
2021-03-19 19:54:04 -0400

    dictionaries

--- 5hrPython/more1.py
+++ 5hrPython/more1.py
@@ -9,5 +9,16 @@ days = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday')
 days[2]
 print(days[2])
 
+friend = {
+    "name":"Jim",
+    "age":25,
+    "gender":"male",
+    "weight":175
+}
+
+print(friend)
+print(friend["name"])
+print(friend.get("name"))
+
 
 
