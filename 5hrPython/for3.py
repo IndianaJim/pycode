2880cdaae7c5d2d0cb17ba9f9349c5768effe7ca
indianajim <jimrunkel@gmail.com>
2021-03-25 20:39:18 -0400

    for again

---
+++ 5hrPython/for3.py
@@ -0,0 +1,9 @@
+
+
+budget = 100
+sandwich_price = 5
+
+while budget > 0:
+    print(f"spent ${sandwich_price}, now budget = ${budget}")
+    budget -= sandwich_price
+
---
+++ 5hrPython/forloop2.py
@@ -0,0 +1,7 @@
+budget = 100
+sandwich_price = 5
+#lunches = list(range(5,100)[5])
+
+while budget > 0:
+    Print(f"{budget}")
+    budget -= 5
