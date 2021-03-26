8a3685132c33201dcce8fb0c05b7308f9a2b9eaf
indianajim <jimrunkel@gmail.com>
2021-03-25 20:10:06 -0400

    forloops

--- 5hrPython/forloop1.py
+++ 5hrPython/forloop1.py
@@ -3,3 +3,13 @@ friends = ["Allen", "Ben", "John"]
 for friend in friends:
     print(f"{friend} is my friend")
 
+best_friend = "Jim"
+count = 0
+
+for friend in friends:
+    count = count + 1
+    if friend == best_friend:
+        print(f"best friend {friend} is in the list")
+    elif count == len(friends):
+        print(f"best friend {best_friend} is NOT in the list.")
+
