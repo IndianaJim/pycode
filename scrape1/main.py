0345c3e2c019ab4773523cd3e052142bf45eb950
indianajim <jimrunkel@gmail.com>
2021-03-31 21:06:44 -0400

    scrape1

--- scrape1/main.py
+++ scrape1/main.py
@@ -7,8 +7,11 @@ import requests
 url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
 res = requests.get(url)
 html_text = res.content
-print(html_text)
 
-#soup = BeautifulSoup(html_text, 'lxml')
+soup = BeautifulSoup(html_text, 'lxml')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+print(company_name)
+#print(job)
 
 
