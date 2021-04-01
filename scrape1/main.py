47707cffe543ebe43f890d2ce6c056edea2f3880
indianajim <jimrunkel@gmail.com>
2021-03-31 21:46:36 -0400

    scrape2

--- scrape1/main.py
+++ scrape1/main.py
@@ -9,9 +9,16 @@ res = requests.get(url)
 html_text = res.content
 
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-print(company_name)
-#print(job)
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+    if 'few' in published_date:
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+        print(f'''
+        Company Name: {company_name}
+        Required Skills: {skills}
+        ''')
+
 
 
