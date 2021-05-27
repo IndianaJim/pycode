40ee2d2449d7937ce22f1fb36644a76c0990ed6b
indianajim <jimrunkel@gmail.com>
2021-05-26 22:03:53 -0400

    more crypto price app

--- dp-streamlit01/cryptoprice.py
+++ dp-streamlit01/cryptoprice.py
@@ -3,8 +3,6 @@ from PIL import Image
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -15,4 +13,15 @@ st.set_page_config(layout="wide")
 # Title
 image = Image.open('bitcoinlogo1.png')
 st.image(image, width=500)
+st.title("Crypto Price App")
+st.markdown("""
+This app retrieves current prices for top 100 crypto tokens.
+""")
+
+# Page layout - continued
+col1 = st.sidebar
+col2, col3 = st.beta_columns((2,1))
+
+#sidebar + main panel
+col1.header('imput options')
 
