333b308ab3506e52b961498342dbd7c4ee0f3394
indianajim <jimrunkel@gmail.com>
2021-06-03 19:32:07 -0400

    priceapp0603732

--- dp-streamlit01/cryptoprice.py
+++ dp-streamlit01/cryptoprice.py
@@ -32,5 +32,12 @@ currency_price_unit = col1.selectbox('Select currency for price',('USD','BTC','E
 @st.cache
 def load_data():
     cmc = requests.get('https://coinmarketcap.com')
-    soup = BeautifulSoup(cmc,content,'html.paser')
+    soup = BeautifulSoup(cmc,content,'html.parser')
+    data = soup.find('script', id='__NEXT_DATA__',type='application/json')
+    coins={}
+    coin_data = json.loads(data.contents[0])
+    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+    
+
+
     
