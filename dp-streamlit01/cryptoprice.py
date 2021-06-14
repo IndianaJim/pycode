01049b85995134ac68fdff83bd655e4286837fc5
indianajim <jimrunkel@gmail.com>
2021-06-13 21:20:38 -0400

    crypto scrape 6-13 2120

--- dp-streamlit01/cryptoprice.py
+++ dp-streamlit01/cryptoprice.py
@@ -34,9 +34,10 @@ def load_data():
     cmc = requests.get('https://coinmarketcap.com')
     soup = BeautifulSoup(cmc.content,'html.parser')
     data = soup.find('script', id='__NEXT_DATA__',type='application/json')
-    coins={}
+    coins= {}
     coin_data = json.loads(data.contents[0])
-    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+    listings= coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+    #print(listings)
     for i in listings:
         coins[str(i['id'])] = i['slug']
     
@@ -53,24 +54,25 @@ def load_data():
         coin_name.append(i['slug'])
         coin_symbol.append(i['symbol'])
         price.append(i['quote'][currency_price_unit]['price'])
-        #percent_change_1h.append(i['quote'][currency_price_unit]['percent_change_1h'])
-        percent_change_24h.append(i['quote'][currency_price_unit]['percent_change_24h'])
-        percent_change_7d.append(i['quote'][currency_price_unit]['percent_change_7d'])
-        market_cap.append(i['quote'][currency_price_unit]['market_cap'])
-        volume_24h.append(i['quote'][currency_price_unit]['volume_24h'])
+        percent_change_1h.append(i['quote'][currency_price_unit]['percentChange1h'])
+        percent_change_24h.append(i['quote'][currency_price_unit]['percentChange24h'])
+        percent_change_7d.append(i['quote'][currency_price_unit]['percentChange7d'])
+        market_cap.append(i['quote'][currency_price_unit]['marketCap'])
+        volume_24h.append(i['quote'][currency_price_unit]['volume24h'])
 
     df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
     df['coin_name'] = coin_name
     df['coin_symbol'] = coin_symbol
     df['price'] = price
-    #df['percent_change_1h'] = percent_change_1h
-    df['percent_change_24h'] = percent_change_24h
-    df['percent_change_7d'] = percent_change_7d
-    df['market_cap'] = market_cap
-    df['volume_24h'] = volume_24h
+    df['percent_change_1h'] = percent_change_1h
+    #df['percent_change_24h'] = percent_change_24h
+    #df['percent_change_7d'] = percent_change_7d
+    #df['market_cap'] = market_cap
+    #df['volume_24h'] = volume_24h
     return df
 
 df = load_data()
+#print(df.prettify())
 
 ## Sidebar - Cryptocurrency selections
 sorted_coin = sorted( df['coin_symbol'] )
