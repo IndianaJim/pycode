f76fe31fe041b8bc936337dd1975b2ccd7365018
indianajim <jimrunkel@gmail.com>
2021-08-05 21:58:05 -0400

    more word def

--- PyMegaApps/worddef/worddef.py
+++ PyMegaApps/worddef/worddef.py
@@ -1,9 +1,16 @@
 import json
+import difflib
+
+from difflib import SequenceMatcher, get_close_matches
 
 data = json.load(open("d:/py/pycode/PyMegaApps/worddef/data.json"))
 
 def translate(w):
-    return data[w]
+    if w in data:
+        return data[w]
+    else:
+        close = get_close_matches(w,data.keys())[0]
+        return "Word not found, maybe try the word: " + close
 
-word = input("enter word to get definition:")
+word = (input("enter word to get definition: ")).lower()
 print(translate(word))
