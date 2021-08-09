6bce3d32d04787d48e8ed8cd100b3081af700054
indianajim <jimrunkel@gmail.com>
2021-08-08 21:45:59 -0400

    word def2a

--- PyMegaApps/worddef/worddef.py
+++ PyMegaApps/worddef/worddef.py
@@ -16,8 +16,18 @@ def translate(w):
             if answer.lower() == "y":
                 return data[get_close_matches(w,data.keys())[matchcounter]]
             matchcounter += 1
+        return "No result found"
     else:
         return "Word not found"
 
-word = (input("enter word to get definition: ")).lower()
-print(translate(word))
+word = (input("\nEnter word to get definition: ")).lower()
+results = translate(word)
+resultcounter = 0
+if isinstance(results, list):
+    print("\nDefinition(s):")
+    while resultcounter < len(results):
+        print(("%s. " % (resultcounter + 1)) + results[resultcounter]) 
+        resultcounter += 1
+else:
+    print(results)
+    
