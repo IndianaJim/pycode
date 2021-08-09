94b51a3fa9b16756f1c79577ffb7888e04174ee6
indianajim <jimrunkel@gmail.com>
2021-08-08 21:07:37 -0400

    word-def2

--- PyMegaApps/worddef/worddef.py
+++ PyMegaApps/worddef/worddef.py
@@ -6,11 +6,18 @@ from difflib import SequenceMatcher, get_close_matches
 data = json.load(open("d:/py/pycode/PyMegaApps/worddef/data.json"))
 
 def translate(w):
+    nummatches = len(get_close_matches(w,data.keys()))
     if w in data:
         return data[w]
+    elif nummatches > 0:
+        matchcounter = 0
+        while matchcounter < nummatches:
+            answer = input("Did you mean %s instead? Enter y or n: " % get_close_matches(w,data.keys())[matchcounter])
+            if answer.lower() == "y":
+                return data[get_close_matches(w,data.keys())[matchcounter]]
+            matchcounter += 1
     else:
-        close = get_close_matches(w,data.keys())[0]
-        return "Word not found, maybe try the word: " + close
+        return "Word not found"
 
 word = (input("enter word to get definition: ")).lower()
 print(translate(word))
