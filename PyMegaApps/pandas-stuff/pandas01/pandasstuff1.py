d77d0fe41c37307cbc4576cbe8354c6b36bef97f
indianajim <jimrunkel@gmail.com>
2021-08-11 21:34:14 -0400

    df02

--- PyMegaApps/pandas-stuff/pandas01/pandasstuff1.py
+++ PyMegaApps/pandas-stuff/pandas01/pandasstuff1.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-d = {'col1': [1, 2], 'col2': [3, 4]}
+#d = {'col1': [1, 2], 'col2': [3, 4]}
+#df = pd.DataFrame(data=d)
 
-df = pd.DataFrame(data=d)
+df = pd.DataFrame([[2,4,6], [20,30,40]],columns=["Price","Count","Value"])
+#df = pd.DataFrame([["Jim","Johnson"], ["James","Smith"]],columns=["First","Last"])
+#df = pd.DataFrame({'FirstName': ["Bob", "John"], 'LastName': ["Johnson", "Jackson"]})
 print(df)
+
+
+print(df.mean())
