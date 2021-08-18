8d8fa2ad1d06af17c3264f92da18e49663474e79
indianajim <jimrunkel@gmail.com>
2021-08-17 21:11:54 -0400

    numpy2

--- PyMegaApps/pandas-stuff/pandas01/geopy1.py
+++ PyMegaApps/pandas-stuff/pandas01/geopy1.py
@@ -1,6 +1,11 @@
 from geopy.geocoders import ArcGIS
+import pandas as pd
+
 nom = ArcGIS()
 
-myLoc = nom.geocode("7723 e 75th st, indianapolis, in")
+#myLoc = nom.geocode("7723 e 75th st, indianapolis, in")
 
-print(nom.geocode("7723 e 75th st, indianapolis, in"))
+df=pd.read_csv("D:\py\pycode\PyMegaApps\pandas-stuff\pandas01\supermarkets.csv")
+
+
+print(df)
