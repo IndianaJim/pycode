cc9cb9ccf5e02742e008f7ec99feb0965efdac16
indianajim <jimrunkel@gmail.com>
2021-08-22 21:11:58 -0400

    folium1

--- PyMegaApps/volcanoes/volcano1.py
+++ PyMegaApps/volcanoes/volcano1.py
@@ -5,7 +5,10 @@ df1=pd.read_csv("Volcanoes.txt")
 m = folium.Map(location=[39.890343205591996, -86.03180675907001])
 lats = list(df1["LAT"])
 lons = list(df1["LON"])
-for lt, ln in zip(lats, lons):
-    m.add_child(folium.Marker(location=[lt, ln], popup="Volcano!", icon=folium.Icon(color='orange')))
+names = list(df1["NAME"])
+statuses = list(df1["STATUS"])
+
+for lt, ln, name, status in zip(lats, lons, names, statuses):
+    m.add_child(folium.Marker(location=[lt, ln], popup=(name + ", " + status), icon=folium.Icon(color='orange')))
 
 m.save("map1.html")
