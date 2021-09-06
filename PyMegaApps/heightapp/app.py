a70be3de2dff202ea75556f3f4c8b1789c0f4c82
indianajim <jimrunkel@gmail.com>
2021-09-05 22:06:58 -0400

    heightapp7

--- PyMegaApps/heightapp/app.py
+++ PyMegaApps/heightapp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from Flask.ext.sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost/height_collector'
@@ -8,8 +8,12 @@ db=SQLAlchemy(app)
 class Data(db.Model):
     __tablename__="data"
     id=db.Column(db.Integer,primary_key=True)
-    
+    email_=db.Column(db.String(120),unique=True)
+    height_=db.Column(db.Integer)
 
+    def __init__(self, email_, height_):
+        self.email_=email_
+        self.height_=height_
 
 @app.route('/')
 def index():
@@ -20,8 +24,13 @@ def success():
     if request.method=='POST':
         email=request.form['email_name']
         height=request.form['height_name']
-        #print(email, height)
-        return render_template('success.html')
+        print(email, height)
+        if db.session.query(Data).filter(Data.email_==email).count() == 0:
+            data=Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+    return render_template('index.html', text='this email has already been logged')
 
 if __name__ == '__main__':
     app.debug = True
