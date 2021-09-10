48b98394ca06f8625eeff0aa266500f4ae9d34c3
indianajim <jimrunkel@gmail.com>
2021-09-09 23:09:37 -0400

    krakenbot2

--- tradingbot-halden/bot.py
+++ tradingbot-halden/bot.py
@@ -4,11 +4,22 @@ import time
 import datetime
 import calendar
 
-api = krakenex.API()
-api.load_key('d:\\py\\apikeys\\kraken.key')
-pair='XETHZUSD'
-since=str(int(time.time() - 3600))
+def get_crypto_data(pair, since):
+    return api.query_public('OHLC', data = {'pair': pair, 'since': since})['result'][pair]
 
-print(json.dumps(api.query_public('OHLC', data = {'pair': pair, 'since': since}), indent=4))
+def get_balance():
+    return api.query_private('Balance') #remove if no balance yet...  ['result']
+
+def get_traddes_history():
+    return 0
+
+if __name__ == '__main__':
+    api = krakenex.API()
+    api.load_key('d:\\py\\apikeys\\kraken.key')
+    pair='XETHZUSD'
+    since=str(int(time.time() - 3600))
+    #print(json.dumps(get_crypto_data(pair, since), indent=4))
+    print(json.dumps(get_balance(), indent=4))
+
 
 
