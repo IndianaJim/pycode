bd71702398d7d28d646835447df47e7e8bab6cca
indianajim <jimrunkel@gmail.com>
2021-10-14 22:22:34 -0400

    algo-chart03

--- algo-streamlit-charting/dashboard.py
+++ algo-streamlit-charting/dashboard.py
@@ -11,6 +11,13 @@ dropdown = st.multiselect('Pick your asset', tickers)
 start = st.date_input('Start',value = pd.to_datetime('2021-01-01'))
 end = st.date_input('End',value = pd.to_datetime('today'))
 
+def relativeret(df):
+    rel = df.pct_change()
+    cumret = (1+rel).cumprod() - 1
+    cumret = cumret.fillna(0)
+    return cumret
+
 if len(dropdown) > 0:
-    df = yf.download(dropdown, start, end)['Adj Close']
+    #df = yf.download(dropdown, start, end)['Adj Close']
+    df = relativeret(yf.download(dropdown, start, end)['Adj Close'])
     st.line_chart(df)
