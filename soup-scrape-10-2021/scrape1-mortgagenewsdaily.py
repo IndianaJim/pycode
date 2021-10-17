e8be6f0205a7566c59da4560729820048bb87ce9
indianajim <jimrunkel@gmail.com>
2021-10-16 21:38:39 -0400

    soup-scrape2

---
+++ soup-scrape-10-2021/scrape1-mortgagenewsdaily.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.mortgagenewsdaily.com/mortgage_rates/daily.aspx"
+response = requests.get(url)
+html = response.text
+#print(html)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+# find all url links on page
+#links = soup.findAll("a")
+#for link in links:
+    #print(link.get("href"))
+
+ThirtyYearRate = soup.find('td', {'class' : 'RateListItem rate current'}) 
+print(ThirtyYearRate.text)
+
