14a468b49500d665530ee60bcb4b41a8359a84dd
indianajim <jimrunkel@gmail.com>
2021-11-02 08:18:33 -0400

    ccxt 3

--- ccxt1/ccxtbot.py
+++ ccxt1/ccxtbot.py
@@ -1,5 +1,41 @@
 import ccxt
 
+def style(s, style):
+    return style + s + '\033[0m'
+
+
+def green(s):
+    return style(s, '\033[92m')
+
+
+def blue(s):
+    return style(s, '\033[94m')
+
+
+def yellow(s):
+    return style(s, '\033[93m')
+
+
+def red(s):
+    return style(s, '\033[91m')
+
+
+def pink(s):
+    return style(s, '\033[95m')
+
+
+def bold(s):
+    return style(s, '\033[1m')
+
+
+def underline(s):
+    return style(s, '\033[4m')
+
+
+def dump(*args):
+    print('\n')
+    print(' '.join([str(arg) for arg in args]))
+
 exchanges = ccxt.exchanges
 
 #for exchange in exchanges:
@@ -7,4 +43,18 @@ exchanges = ccxt.exchanges
 
 exchange = ccxt.coinbasepro()
 
-print(exchange.)
+symbol = 'BTC/USD'
+ticker = exchange.fetch_ticker(symbol.upper())
+print(ticker)
+dump(
+    green(exchange.id),
+    yellow(symbol),
+    'ticker',
+    ticker['datetime'],
+    'high: ' + str(ticker['high']),
+    'low: ' + str(ticker['low']),
+    'bid: ' + str(ticker['bid']),
+    'ask: ' + str(ticker['ask']),
+    'volume: ' + str(ticker['quoteVolume']))
+
+
